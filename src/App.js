34d3d8bc56328de98207aeafb45774540da4d32a
Mugeha <mugehajacky@gmail.com>
2025-04-18 23:08:40 +0300

    Fifth commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-// App.js
+// 1. Import React and the custom hook to access comments
 import React from 'react';
+import AddComment from './Components/AddComment';
 import { useComments } from './Context/CommentsContext';
-import './App.css'; 
 
 const App = () => {
+  // 2. Pull comments from our global context using the custom hook
   const { comments } = useComments();
 
   return (
+    <>
     <main className="app">
       <h1 className="title">💬 Interactive Comments Section</h1>
 
@@ -21,6 +23,8 @@ const App = () => {
         </div>
       ))}
     </main>
+    <AddComment />
+    </>
   );
 };
 
